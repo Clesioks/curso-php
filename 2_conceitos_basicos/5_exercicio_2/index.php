1862098d264c62696dfd0e30c6e38cf7e98d12a1
Clesioks <kornalewski@outlook.com>
2022-08-16 19:06:23 -0300

    final sec 2

---
+++ 2_conceitos_basicos/5_exercicio_2/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// variável com nome da pessoa
+$nome = 'TiTi';
+
+// variável com o peso
+
+$peso = 89;
+
+// variável com a altura
+
+$altura = 1.77;
+
+echo "Essa pessoa tem o nome " . $nome . " e o peso " . $peso . " com altura " . $altura;
+
+
+?>
