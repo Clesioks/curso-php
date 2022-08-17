74c7a0b8357e9e70b500f23dcec01c3b4976b958
Clesioks <kornalewski@outlook.com>
2022-08-17 15:12:53 -0300

    tipo de dados

---
+++ 3_tipos_de_dados/5_checando_float/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$a = 8.12;
+
+$b = "teste";
+
+
+if(is_float($a)){
+    echo "é float! variável a";
+}
+
+
+if(is_float($b)){
+    echo "é float!";
+}
---
+++ 3_tipos_de_dados/6_exercicios_float/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 1.12;
+$b = 2.45;
+$c = 5.54;
+
+echo $a . "<br>";
+echo $b . "<br>";
+echo $c . "<br>";
+
+if(is_float($a)){
+    echo "É float";
+}
