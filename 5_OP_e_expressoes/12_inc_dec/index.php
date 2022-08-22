8f1675808a232eedda1ed352a008630e17e2b4b1
Clesioks <kornalewski@outlook.com>
2022-08-22 15:17:10 -0300

    operadores

---
+++ 5_OP_e_expressoes/12_inc_dec/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$x = 0;
+$y = 0;
+
+$x++;
+
+$y = $y + 1;
+
+
+
+echo $x;
+echo "<br>";
+echo $y;
+
+$n = 10;
+$m = 50;
+
+$n--;
+
+$m = $m - 1;
+echo "<br>";
+echo $n;
+echo "<br>";
+echo $m;
