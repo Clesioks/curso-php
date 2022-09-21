559d70740200307937a9ca13c4c0a681ec16781e
Clesioks <kornalewski@outlook.com>
2022-09-21 20:29:21 -0300

    agenda

---
+++ 17_agenda/config/process.php
@@ -0,0 +1,94 @@
+<?php
+
+session_start();
+
+include_once("connection.php");
+include_once("url.php");
+
+$data = $_POST;
+
+
+// MODIFICAÇÕES NO BANCO
+if(!empty($data)) {
+
+
+    //criar contato
+    if($data["type"] === "create"){
+
+        $name = $data["name"];
+        $phone = $data["phone"];
+        $observations = $data["observations"];
+
+        $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+        
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":phone", $phone);
+        $stmt->bindParam(":observations", $observations);
+
+        try {
+
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato criado com sucesso!";
+
+
+} catch(PDOException $e){
+    $error = $e->getMessage();
+    echo "Erro: $error";
+}
+    }
+
+    // redirect home
+
+    header("Location:" . $BASE_URL . "../index.php");
+
+
+    // SELEÇÃO DE DADOS
+} else {
+
+    $id;
+
+if(!empty($_GET)) {
+    $id = $_GET["id"];
+}
+    // retorna um contato
+
+if(!empty($id)) {
+
+    $query = "SELECT * FROM contacts WHERE id = :id";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":id", $id);
+
+    $stmt->execute();
+
+    $contact = $stmt->fetch();
+
+} else {
+
+    // retorna todos contatos
+    $contacts = [];
+    
+    $query = "SELECT * FROM contacts";
+    
+    $stmt = $conn->prepare($query);
+    
+    $stmt->execute();
+    
+    $contacts = $stmt->fetchAll();
+}
+
+}
+
+// FECHAR CONEXAO
+
+$conn = null;
+
+
+
+
+
+
+?>
